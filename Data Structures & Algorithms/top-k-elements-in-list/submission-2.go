2a0613bb60f06823fb6465699608d8fe2ca11d9b
Aaditya B <150529501+aadityabinodyadav@users.noreply.github.com>
2026-04-12 01:51:42 +0545

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.go
@@ -0,0 +1,17 @@
+func topKFrequent(nums []int, k int) []int {
+		freq := make(map[int]int)
+
+		for _, num := range nums {
+			freq[num]++
+		}
+    unique := make([]int, 0)
+    for num := range freq {
+        unique = append(unique, num)
+    }
+
+    sort.Slice(unique, func(i, j int) bool {
+        return freq[unique[i]] > freq[unique[j]]
+    })
+
+    return unique[:k]
+}
