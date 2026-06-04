c82f79b5991f44de2153773fa2266ec70cea4704
Aaditya B <150529501+aadityabinodyadav@users.noreply.github.com>
2026-06-04 22:35:09 +0545

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    postorderTraversal(root) {
+        let result = [];
+
+        function dfs(node) {
+            if (!node) return;
+
+
+            dfs(node.left);        
+            dfs(node.right);       
+            result.push(node.val); 
+        }
+
+        dfs(root);
+
+        return result;
+    }
+}
