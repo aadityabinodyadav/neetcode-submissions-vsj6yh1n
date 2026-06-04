d5c072db4b04e9db7867a38459e96670eed815e6
Aaditya B <150529501+aadityabinodyadav@users.noreply.github.com>
2026-06-04 12:48:35 +0545

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.js
@@ -0,0 +1,17 @@
+class MyHashSet {
+    constructor() {
+        this.set = new Set();
+    }
+
+    add(key) {
+        this.set.add(key);
+    }
+
+    remove(key) {
+        this.set.delete(key);
+    }
+
+    contains(key) {
+        return this.set.has(key);
+    }
+}
